6dc5d9dbd8bbe0b19cc6d23477b8459b24b8d9b5
Weslley Araujo <weslleyaraujo20@gmail.com>
2014-07-30 00:29:45 -0300

    Not equals is nice!

--- dist/testsuite.js
+++ dist/testsuite.js
@@ -137,3 +137,22 @@
   }
 
 }(this.Test, this.Test.methods);
+
+!function(Test, Methods) {
+  'use strict';
+
+  Test.prototype.notEquals = function () {
+    var _args = Methods.toArray.apply(null, arguments);
+
+    // does it equals?
+    if (!Methods.eql.apply(null, _args)) {
+      this.report.success++;
+      return true;
+    }
+
+    this.report.failures++;
+    this.report.errorMessages.push('Expected ' + _args[0] + ' not to be ' + _args[1]);
+    return false;
+  }
+
+}(this.Test, this.Test.methods);
